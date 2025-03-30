0e12f018e6f67dfdf6ce61026cd687382d0d155b
Kaisa Korhonen <kaisa@xd.fi>
2025-03-30 16:10:11 +0300

    feat: update user authentication

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-import users, db, config
+"""Module for application routes"""
+import sqlite3
 from flask import Flask
-from flask import render_template,  redirect, request
+from flask import flash, render_template, redirect, request, session
+import users, recipes, config
 
 app = Flask(__name__)
 app.secret_key = config.secret_key
@@ -10,8 +12,10 @@ def index():
     """
     Frontpage view 
     """
-    options = ["Tallentaa ja poistaa omia reseptejä", "Hakea, kommentoida ja lisätä suosikeiksi omia sekä muiden reseptejä", ]
-    return render_template("index.html", message="Tervetuloa!", intro="Tällä sovelluksella voit:", items=options, 
+    options = ["Tallentaa ja poistaa omia reseptejä", "Hakea, kommentoida \
+               ja lisätä suosikeiksi omia sekä muiden reseptejä"]
+    return render_template("index.html", message="Tervetuloa!", \
+                           intro="Tällä sovelluksella voit:", items=options,
                            reminder="Kirjauduthan sisään käyttääksesi toimintoja! :)")
 
 @app.route("/page")
@@ -19,30 +23,42 @@ def home_page():
     """
     Personal page
     """
-    options = [("Haku", "/page"), ("Lisää resepti", "/page"), ("Omat reseptit", "/page"), ("Suosikit", "/page")]
-    return render_template("page.html", message="Tervetuloa", user="Testi", \
+    user = users.username(users.user_id())
+    options = [("Haku", "/page"), ("Lisää resepti", "/page"), \
+               ("Omat reseptit", "/page"), ("Suosikit", "/page")]
+    return render_template("page.html", message="Tervetuloa", user=user, \
                            intro="Tällä sivulla voit luoda uusia reseptejä, tutkia omia sekä \
                             tallentamiasi reseptejä sekä hakea reseptejä:", items=options)
 
 @app.route("/login", methods=["GET","POST"])
 def login():
+    """
+    Login
+    """
     if request.method == "GET":
         return render_template("login.html")
     if request.method == "POST":
         username = request.form["username"]
         password = request.form["password"]
-        if users.login(username, password):
-            return home_page()
+
+        user_id = users.login(username, password)
+
+        if user_id:
+            session["user_id"] = user_id
+            return redirect("/page")
         else:
-            return render_template("error.html", message="Kirjautuminen epäonnistui!")
+            flash("Virhe: Kirjautuminen epäonnistui! Väärä tunnus tai salasana")
+            return render_template("login.html")
 
 @app.route("/logout")
 def logout():
-    users.logout()
+    """Logout"""
+    del session["user_id"]
     return redirect("/")
 
 @app.route("/register", methods=["GET","POST"])
 def register():
+    """Registration"""
     if request.method == "GET":
         return render_template("register.html")
     if request.method == "POST":
@@ -50,8 +66,12 @@ def register():
         password1 = request.form["password1"]
         password2 = request.form["password2"]
         if password1 != password2:
-            return render_template("error.html", message="Salasanat eroavat!")
-        if users.register(username, password1):
+            flash("Virhe: Salasanat eroavat!")
+            return render_template("register.html")
+        try:
+            users.register(username, password1)
+            flash("Tunnuksen luonti onnistui")
             return redirect("/login")
-        else:
-            return render_template("error.html", message="Tunnuksen luonti epäonnistui!")
+        except sqlite3.IntegrityError:
+            flash("Virhe: Tunnus on jo olemassa, kirjaudu sisään tai valitse uusi tunnus")
+            return render_template("register.html")
--- users.py
+++ users.py
@@ -1,41 +1,28 @@
-import sqlite3
-import db
 from flask import session
 from werkzeug.security import check_password_hash, generate_password_hash
-
-def login(username, password):
-    user = db.query_one("SELECT id, password FROM users WHERE username = ?", (username,))
-    
-    if not user:
-        return False
-    else:
-        user_id, hashed_password = user
-        if check_password_hash(hashed_password, password):
-            session["user_id"] = user_id
-            return True
-        else:
-            return False
-
-def logout():
-    session.pop("user_id", None)
+import db
 
 def register(username, password):
-    hash_value = generate_password_hash(password)
-    try:
-        db.execute("INSERT INTO users (username, password) VALUES (?, ?)", (username, hash_value))
-    except sqlite3.IntegrityError as e:
-        print(f"Error: {e}")
-        return False
-    return login(username, password)
+    password_hash = generate_password_hash(password)
+    sql = "INSERT INTO users (username, password_hash) VALUES (?, ?)"
+    db.execute(sql, [username, password_hash])
+
+def login(username, password):
+    sql = "SELECT id, password_hash FROM users WHERE username = ?"
+    result = db.query(sql, [username])
+
+    if len(result) == 1:
+        user_id, password_hash = result[0]
+        if check_password_hash(password_hash, password):
+           return user_id
+       
+    return None
 
 def user_id():
     return session.get("user_id", 0)
 
 def username(id):
-    try:
-        result = db.query_one("SELECT username FROM users WHERE id = ?", (id,))
-        if result:
-            return result[0]
-    except sqlite3.Error as e:
-        print(f"Error: {e}")
+    result = db.query_one("SELECT username FROM users WHERE id = ?", (id,))
+    if result:
+        return result[0]
     return None
